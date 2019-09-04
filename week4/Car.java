cd6925341cccbc0188c417bf5fb908b6dea69ae9
josalguero <josalguero1@gmail.com>
2019-09-04 13:50:14 +0000

    Create CAr. java and its no-args contructor

---
+++ week4/Car.java
@@ -0,0 +1,17 @@
+public class Car
+{
+
+
+    private String model;
+    private Color color;
+    private double tank;
+    private int numPassanger;
+
+    public Car
+    {
+        model= "mustang"
+        color = .YELLOW
+        tank = 20.3
+        numPassangers = 2
+    }
+}
