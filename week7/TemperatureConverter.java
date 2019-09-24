eeb995894e14c6aed96b403eecc08e0a4b6b63ba
josalguero <josalguero1@gmail.com>
2019-09-24 14:15:23 +0000

    converts celsiums into fahrenheit

--- week7/TemperatureConverter.java
+++ week7/TemperatureConverter.java
@@ -3,28 +3,50 @@ public class TemperatureConverter
     private double temperature;
     private boolean isCelsius;
 
-    public TemperatureConverter (double celcius)
+    public TemperatureConverter (double celsius)
     {
         temperature = celsius;
         isCelsius = true;
     }
 
     public void convertToF()
-
-    if (isCelsius)
     {
-        temperature = temperature * 9/5 + 32;
+        if (isCelsius)
+        {
+            temperature = temperature * 9/5 + 32;
+            isCelsius = false;
+        }
+    }
+
+    public void convertToC()
+    {
+        if (!isCelsius)
+        {
+            temperature = (temperature - 32) * 5/9;
+            isCelsius = true;
+        }
     }
     public String toString()
     {
-        String result = "the current temperature is " + temperature + "degrees";
-        if(isCelsius)
+        String result = "the current temperature is " + temperature + " degrees";
+        if (isCelsius)
         {
-            result += " celsius";
+            result += " celsius ";
         }
         else
         {
-            result += " Fahrenheit"
+            result += " Fahrenheit ";
         }
         return result;
+    }
+
+    public static void main (String[]args)
+    {
+        TemperatureConverter myTemp = new TemperatureConverter(25.6);
+        System.out.println(myTemp);
+        myTemp.convertToF();
+        System.out.print(myTemp);
+        myTemp.convertToC();
+        System.out.println(myTemp);
+    }
 }
